dfbd6d9e69fd3525728ebf9e197665767d3b32b5
gfaleiro <faleirogabrielf@gmail.com>
2021-04-11 12:02:58 +0200

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,3 +1,2 @@
-sudo docker swarm init
-sudo docker stack deploy -c docker-compose_rbpi4_v2.yml --resolve-image "always" homeautomation
-
+docker swarm init
+docker stack deploy -c docker-compose.yml --resolve-image "always" homeautomation
