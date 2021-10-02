eab3083ce04a1dab745884cceb065d67ca59b8c3
Gabriel Faleiro <faleirogabrielf@gmail.com>
2021-10-02 23:28:36 +0200

    Use emqx-edge as the mqtt broker of the application

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export DOCKER_CONFIG_DIR=$PWD
+export ARCH=amd64
+# export ARCH=arm64v8
 
 docker swarm init
 docker stack deploy -c docker-compose.yml --resolve-image "always" homeautomation
